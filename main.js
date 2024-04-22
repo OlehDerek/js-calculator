934dc411d9cd15af776ddf16024ad90614549055
OlehDerek <157998206+OlehDerek@users.noreply.github.com>
2024-04-22 17:42:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function clearDisplay() {
+  document.getElementById('display').value = '0';
+ 
+}
+
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+var op = operand1.toString();
+
+if (op.length > 1 && op[1]!= ".") {
+  op.shift();
+}
+
+function calculateResult() {
+  var displayValue = document.getElementById('display').value;
+  var result;
+  var match = displayValue.match(/([\d.]+)([*/+-])([\d.]+)/);
+// make that erase zero
+// no double dots
+// more digits in display
+
+  if (match) {
+      var operand1 = parseFloat(match[1]);
+      var operator = (match[2]);
+      var operand2 = parseFloat(match[3]);
+
+      switch (operator) {
+          case '+':
+              result = operand1 + operand2;
+              break;
+          case '-':
+              result = operand1 - operand2;
+              break;
+          case '*':
+              result = operand1 * operand2;
+              break;
+          case '/':
+              result = operand1 / operand2;
+              break;
+          default:
+              result = 'Error';
+      }
+  }
+  
+  else {
+    result = 'Error';
+ }
+
+  document.getElementById('display').value = result;
+}
